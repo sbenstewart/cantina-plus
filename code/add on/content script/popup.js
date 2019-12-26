1d6c41dda5dd029ed6de4fe40fef607180c5c04d
sbenstewart <sbenstewart@gmail.com>
2019-12-26 23:18:35 +0530

    content script popup updated

--- code/add on/content script/popup.js
+++ code/add on/content script/popup.js
@@ -5,7 +5,7 @@ function transfer(){
 	var tablink;
 	chrome.tabs.getSelected(null,function(tab) {
 	   	tablink = tab.url;
-		$("#p1").text("The URL being tested is - "+tablink);
+		$("#p1").text("The URL being tested is "+tablink);
 
 		var xhr=new XMLHttpRequest();
 		params="url="+tablink;
