9cda15ed08adfbd389c5a32374f58e942142e669
sbenstewart <sbenstewart@gmail.com>
2019-12-28 16:32:04 +0530

    implementation

--- code/add on/content script/popup.js
+++ code/add on/content script/popup.js
@@ -5,18 +5,17 @@ function transfer(){
 	var tablink;
 	chrome.tabs.getSelected(null,function(tab) {
 	   	tablink = tab.url;
-		$("#p1").text("The URL being tested is "+tablink);
 
 		var xhr=new XMLHttpRequest();
 		params="url="+tablink;
-        // alert(params);
+        //alert(params);
 		var markup = "url="+tablink+"&html="+document.documentElement.innerHTML;
-		xhr.open("POST","http://localhost/Malicious-Web-Content-Detection-Using-Machine-Learning/clientServer.php",true);
+		xhr.open("POST","http://localhost:8888/clientServer.php",false);
 		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 		xhr.send(markup);
 		// Uncomment this line if you see some error on the extension to see the full error message for debugging.
-		// alert(xhr.responseText);
-		$("#div1").text(xhr.responseText);
+		alert(xhr.responseText);
+		//$("#div1").text(xhr.responseText);
 		return xhr.responseText;
 	});
 }
