5c3c092ec080b777fdb2cf257351402d5cc6c6e0
sbenstewart <sbenstewart@gmail.com>
2020-02-28 20:01:24 +0530

    whitelist redundancy fixed

--- code/phish detector/whitelistadder.py
+++ code/phish detector/whitelistadder.py
@@ -4,11 +4,16 @@ import json
 def main():
     url = sys.argv[1]
 
-    white_list_file=open('/Users/benstewart/BEN STUFF/PROJECTS/fyp/code/phish detector/whitelist.txt', "a+")
-    white_list_file.write(url)
-    white_list_file.write("\n")
-    white_list_file.close()
-    print("Whitelist Updated")
+    white_list_file = open('/Users/benstewart/BEN STUFF/PROJECTS/fyp/code/phish detector/whitelist.txt').read()
+    white_list = white_list_file.split('\n')
+    if url not in white_list:
+        white_list_file=open('/Users/benstewart/BEN STUFF/PROJECTS/fyp/code/phish detector/whitelist.txt', "a+")
+        white_list_file.write(url)
+        white_list_file.write("\n")
+        white_list_file.close()
+        print("Whitelist Updated")
+    else:
+        print("Already present in Whitelist")
 
 if __name__ == "__main__":
     main()
