fe53b79da5c2329bfc90ca62918fd5d48b43cc7f
Ben Stewart <sbenstewart@gmail.com>
2020-07-26 15:04:39 +0530

    mount url changed

--- code/phish detector/updatewhitelist.php
+++ code/phish detector/updatewhitelist.php
@@ -2,6 +2,6 @@
 header("Access-Control-Allow-Origin: *");
 $site=$_POST['url'];
 
-$decision=exec("/Users/benstewart/anaconda3/envs/py2/bin/python /Users/benstewart/BEN\ STUFF/PROJECTS/fyp/code/phish\ detector/whitelistadder.py $site 2>&1 ");
+$decision=exec("/mnt/neverdelete/projects/fyp/code/phish\ detector/whitelistadder.py $site 2>&1 ");
 echo $decision;
 ?>
