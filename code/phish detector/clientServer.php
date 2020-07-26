47e7fd3bff1534f9db1b4278eb29be92602486b2
Ben Stewart <sbenstewart@gmail.com>
2020-07-26 15:40:32 +0530

    more fixes

--- code/phish detector/clientServer.php
+++ code/phish detector/clientServer.php
@@ -10,7 +10,7 @@ echo $decision;
 
 if($decision == "PHISHING")
     {
-        $decision=exec("/mnt/neverdelete/projects/fyp/code/target\ identifier/scrape.py http://localhost:8050 /Users/benstewart/BEN\ STUFF/PROJECTS/fyp/code/target\ identifier/single-sites.json 1 /Users/benstewart/BEN\ STUFF/PROJECTS/fyp/code/target\ identifier/data 2>&1 ");
+        $decision=exec("/mnt/neverdelete/projects/fyp/code/target\ identifier/scrape.py http://localhost:8050 /mnt/neverdelete/projects/fyp/code/target\ identifier/single-sites.json 1 /mnt/neverdelete/projects/fyp/code/target\ identifier/data 2>&1 ");
         $decision=exec("/mnt/neverdelete/projects/fyp/code/target\ identifier/compare-tags-all.py data 2>&1 ");
         echo $decision;
     }
